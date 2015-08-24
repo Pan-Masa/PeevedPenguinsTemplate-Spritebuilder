8ab3629ddfa179e93e07bcd7aed0dea01df56bef
Pan-Masa <nasmac@NasMacs-MacBook-Air.local>
2015-08-24 14:34:28 +0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/GamePlay.h
@@ -0,0 +1,13 @@
+//
+//  GamePlay.h
+//  PeevedPenguins
+//
+//  Created by Nas@Mac on 24/8/58.
+//  Copyright (c) พ.ศ. 2558 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface GamePlay : CCNode
+
+@end
